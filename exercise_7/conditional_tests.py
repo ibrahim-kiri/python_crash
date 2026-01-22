047530d69aad021a4387cce7b46f1790203e375a
ibrahim-kiri <ibrahimkiri@gmail.com>
2026-01-22 07:37:30 +0300

    Added boolean expressions

---
+++ exercise_7/conditional_tests.py
@@ -0,0 +1,41 @@
+fruit = 'apple'
+if fruit == 'apple':
+    print(True)
+else:
+    print(False)
+
+pizza = 'italian'
+if pizza != 'chicken':
+    print(True)
+else:
+    print(False)
+
+car = 'subaru'
+if car > 'toyota':
+    print(True)
+else:
+    print(False)
+
+animal = 'lion'
+if animal == ('Lion'.lower()):
+    print(True)
+else:
+    print(False)
+
+number = 22
+if number > 33:
+    print(True)
+else:
+    print(False)
+
+names = ['john', 'jane', 'kelly']
+if 'john' and 'jane' in names:
+    print(True)
+else:
+    print(False)
+
+count = [1, 3, 5, 6, 8]
+if 4 in count or 9 in count:
+    print(True)
+else:
+    print(False)
