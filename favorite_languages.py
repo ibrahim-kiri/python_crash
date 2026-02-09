4c91657820a88f156995f0c8a145b4805c84db15
ibrahim-kiri <ibrahimkiri@gmail.com>
2026-02-09 09:45:11 +0300

    looping through keys in a dictionary2

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,5 +5,38 @@ favorite_languages = {
     'phil': 'python',
 }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# Looping through all the keys in a dictionary
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
